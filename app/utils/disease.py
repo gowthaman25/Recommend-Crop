dc3fbac08a4be714c1adc0b0fd15e4aeaa75d712
unknown <ecsgowtham@gmail.com>
2023-05-06 09:50:15 +0530

    Resnet application

---
+++ app/utils/disease.py
@@ -0,0 +1,62 @@
+disease_dic = {
+    'Apple___Apple_scab': """ <b>Crop</b>: Apple <br/>Disease: Apple Scab<br/>
+        <br/> Cause of disease:
+
+        <br/><br/> 1. Apple scab overwinters primarily in fallen leaves and in the soil. Disease development is favored by wet, cool weather that generally occurs in spring and early summer.
+
+        <br/> 2. Fungal spores are carried by wind, rain or splashing water from the ground to flowers, leaves or fruit. During damp or rainy periods, newly opening apple leaves are extremely susceptible to infection. The longer the leaves remain wet, the more severe the infection will be. Apple scab spreads rapidly between 55-75 degrees Fahrenheit.
+        <br/><br/> How to prevent/cure the disease <br/>
+        <br/>1. Choose resistant varieties when possible.
+
+        <br/>2. Rake under trees and destroy infected leaves to reduce the number of fungal spores available to start the disease cycle over again next spring
+        
+        <br/>3. Water in the evening or early morning hours (avoid overhead irrigation) to give the leaves time to dry out before infection can occur.
+        <br/>4. Spread a 3- to 6-inch layer of compost under trees, keeping it away from the trunk, to cover soil and prevent splash dispersal of the fungal spores.""",
+
+    'Apple___Black_rot': """ <b>Crop</b>: Apple <br/>Disease: Black Rot<br/>
+        <br/> Cause of disease:
+
+        <br/><br/>Black rot is caused by the fungus Diplodia seriata (syn Botryosphaeria obtusa).The fungus can infect dead tissue as well as living trunks, branches, leaves and fruits. In wet weather, spores are released from these infections and spread by wind or splashing water. The fungus infects leaves and fruit through natural openings or minor wounds.
+        <br/><br/> How to prevent/cure the disease <br/>
+        <br/>1. Prune out dead or diseased branches.
+
+        <br/>2. Prune out dead or diseased branches.
+        
+        <br/>3. Remove infected plant material from the area.
+        <br/>4. Remove infected plant material from the area.
+        <br/>5. Be sure to remove the stumps of any apple trees you cut down. Dead stumps can be a source of spores.""",
+
+    'Apple___Cedar_apple_rust': """ <b>Crop</b>: Apple <br/>Disease: Cedar Apple Rust<br/>
+        <br/> Cause of disease:
+
+        <br/><br/>Cedar apple rust (Gymnosporangium juniperi-virginianae) is a fungal disease that depends on two species to spread and develop. It spends a portion of its two-year life cycle on Eastern red cedar (Juniperus virginiana). The pathogen’s spores develop in late fall on the juniper as a reddish brown gall on young branches of the trees.
+
+        <br/><br/> How to prevent/cure the disease <br/>
+        <br/>1. Since the juniper galls are the source of the spores that infect the apple trees, cutting them is a sound strategy if there aren’t too many of them.
+
+        <br/>2. While the spores can travel for miles, most of the ones that could infect your tree are within a few hundred feet.
+        
+        <br/>3. The best way to do this is to prune the branches about 4-6 inches below the galls.""",
+
+
+
+    'Apple___healthy': """ <b>Crop</b>: Apple <br/>Disease: No disease<br/>
+
+        <br/><br/> Don't worry. Your crop is healthy. Keep it up !!!""",
+
+
+
+
+    'Blueberry___healthy': """ <b>Crop</b>: Blueberry <br/>Disease: No disease<br/>
+
+        <br/><br/> Don't worry. Your crop is healthy. Keep it up !!!""",
+
+
+
+
+    
+
+    'Raspberry___healthy': """ <b>Crop</b>: Raspberry <br/>Disease: No disease<br/>
+
+        <br/><br/> Don't worry. Your crop is healthy. Keep it up !!!"""
+}
